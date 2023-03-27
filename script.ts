82e0368b170fac86682dfe66eeab689b1184355e
Karolina Chodera <karolinachodera@gmail.com>
2023-03-27 13:52:30 +0200

    add types and debug part of errors

--- script.ts
+++ script.ts
@@ -1,68 +1,80 @@
-"use strict";
-const button = document.querySelector(".add");
-const form = document.querySelector("#adding-items");
-const select = form.querySelector("select");
+const button: HTMLElement = document.querySelector(".add")!;
+const form: HTMLElement = document.querySelector("#adding-items")!;
+const select: HTMLElement = form.querySelector("select")!;
 const quickButtons = document.querySelectorAll(".quick-add button");
-let localStorageItems = localStorage.getItem("items");
-let items = JSON.parse(localStorageItems) || [];
-let index = 0;
-let list;
-let removeListButton;
-function ItemObject(name, category) {
+let localStorageItems: string = localStorage.getItem("items")!;
+let items: ItemObject[] = JSON.parse(localStorageItems) || [];
+let index: number = 0; 
+let list: HTMLElement;
+let removeListButton: HTMLElement;
+
+interface ItemObject {
+    name: string;
+    category: string;
+    id: number;
+    isChecked: boolean;
+}
+
+function ItemObject(name: string, category: string) {
     this.name = name;
     this.category = category;
     this.id = index;
     this.isChecked = false;
 }
-function moveToEnd(e) {
-    let index = items.findIndex(item => item.id == this.dataset.id);
+
+function moveToEnd(e: Event): void{
+    let index: number = items.findIndex(item => item.id == this.dataset.id);
     let itemHtml = this;
     items[index].isChecked = !items[index].isChecked;
     localStorage.setItem("items", JSON.stringify(items));
-    if (items[index].isChecked) {
+
+    if(items[index].isChecked) {
         this.classList.add("checked");
-        e.target.setAttribute("checked", "true");
+        (e.target as HTMLElement).setAttribute("checked", "true");
         removeListButton.before(itemHtml);
-    }
-    else {
+    } else {
         this.classList.remove("checked");
-        e.target.removeAttribute("checked");
-        let checked = list.querySelector("input:checked");
-        if (checked) {
+        (e.target as HTMLElement).removeAttribute("checked");
+        let checked: HTMLElement | null = list.querySelector("input:checked");
+        if(checked) {
             checked.closest("li").before(itemHtml);
         }
     }
 }
-function insertBeforeChecked(html) {
+
+function insertBeforeChecked(html: HTMLElement): void {
     let checked = list.querySelector("input:checked");
-    if (checked) {
+    if(checked) {
         checked.closest("li").before(html);
     }
 }
-function startList(items) {
-    if (items.length > 0) {
+
+function startList(items: ItemObject[]) {
+    if(items.length > 0) {
         createList();
-        list.innerHTML = items.sort((itemA, itemB) => {
+        list.innerHTML = items.sort((itemA, itemB): number => {
             let isCheckedA = itemA.isChecked ? 0 : 1;
             let isCheckedB = itemB.isChecked ? 0 : 1;
             return isCheckedB - isCheckedA;
+
         }).map((item) => {
-            if (item.id >= index) {
+            if(item.id >= index) {
                 index = item.id + 1;
             }
             return `<li data-id="${item.id}" class="${item.category} ${item.isChecked ? 'checked' : ''}">
                 <input type="checkbox" ${item.isChecked ? "checked" : ""}>${item.name}
                 <button class="trash">❌</button>
-            </li>`;
+            </li>`
         }).join("");
         createRemoveButton();
         const listItems = list.querySelectorAll("li");
         listItems.forEach(item => item.addEventListener("change", moveToEnd));
         const trashButtons = list.querySelectorAll(".trash");
         trashButtons.forEach(button => button.addEventListener("click", removeListItem));
-    }
+    } 
 }
-function createList() {
+
+function createList(): void {
     let listHtml = document.createElement("ul");
     listHtml.setAttribute("id", "shopping-list");
     listHtml.setAttribute("class", "box");
@@ -70,7 +82,8 @@ function createList() {
     list = document.querySelector("#shopping-list");
     createRemoveButton();
 }
-function removeList() {
+
+function removeList(): void {
     localStorage.clear();
     list.remove();
     items = [];
@@ -78,7 +91,8 @@ function removeList() {
     list = document.querySelector("#shopping-list");
     removeListButton = document.createElement("button");
 }
-function createRemoveButton() {
+
+function createRemoveButton(): void {
     removeListButton = document.createElement("button");
     removeListButton.classList.add("remove-list");
     removeListButton.classList.add("add");
@@ -86,56 +100,65 @@ function createRemoveButton() {
     list.append(removeListButton);
     removeListButton.addEventListener("click", removeList);
 }
-function createListItem(item) {
+
+function createListItem(item: ItemObject) {
     items.push(item);
     index++;
-    if (items.length === 1 && !list) {
+
+    if(items.length === 1 && !list) {
         createList();
     }
-    const html = document.createElement("li");
+
+    const html: HTMLElement = document.createElement("li");
     html.setAttribute("data-id", `${item.id}`);
     html.classList.add(`${item.category}`);
     html.innerHTML = `<input type="checkbox" ${item.isChecked ? "checked" : ""}>${item.name}
                         <button class="trash">❌</button>`;
-    let checked = list.querySelector("input:checked");
-    if (checked) {
-        checked.parentElement.before(html);
-    }
-    else if (!removeListButton) {
-        list.append(html);
-    }
-    else {
-        removeListButton.before(html);
-    }
-    const listItem = list.querySelector(`li[data-id="${item.id}"]`);
-    const trashButton = listItem.querySelector(".trash");
+
+    let checked: HTMLElement | null = list.querySelector("input:checked");
+
+        if(checked) {
+            checked.parentElement.before(html);
+        } else if(!removeListButton) {
+            list.append(html);
+        } else {
+            removeListButton.before(html);
+        }
+
+    const listItem: HTMLElement = list.querySelector(`li[data-id="${item.id}"]`)!;
+    const trashButton: HTMLElement = listItem.querySelector(".trash")!;
     listItem.addEventListener("change", moveToEnd);
     trashButton.addEventListener("click", removeListItem);
     localStorage.setItem("items", JSON.stringify(items));
 }
-function removeListItem() {
-    let index = items.findIndex(item => item.id == this.parentNode.dataset.id);
+
+function removeListItem(): void {
+    let index: number = items.findIndex(item => item.id == this.parentNode.dataset.id);
     this.parentNode.remove();
     items.splice(index, 1);
     localStorage.setItem("items", JSON.stringify(items));
-    if (items.length === 0) {
+
+    if(items.length === 0) {
         removeList();
     }
 }
-function startItem(e) {
+
+function startItem(e: Event): void {
     e.preventDefault();
-    let name = this.querySelector("input[type='text']").value;
-    let category = select.options[select.selectedIndex].value;
-    const item = new ItemObject(name, category);
+    let name: string = this.querySelector("input[type='text']").value;
+    let category: string = select.options[select.selectedIndex].value;
+    const item: ItemObject = new ItemObject(name, category);
     createListItem(item);
     this.reset();
 }
-function startQuickItem() {
-    let name = this.textContent;
-    let category = this.value;
-    const item = new ItemObject(name, category);
+
+function startQuickItem(): void {
+    let name: string = this.textContent;
+    let category: string = this.value;
+    const item: ItemObject = new ItemObject(name, category);
     createListItem(item);
 }
+
 form.addEventListener("submit", startItem);
 quickButtons.forEach(button => button.addEventListener("click", startQuickItem));
 startList(items);
